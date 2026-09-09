1f092577a24951174bc0d2bb508a44d19951c1ba
Ivica Cardic <ivica.cardic@gmail.com>
2026-09-09 11:42:20 +0200

    5685 client - Show workflow issues on the canvas nodes and in a note

--- client/src/pages/platform/workflow-editor/components/WorkflowEditor.tsx
+++ client/src/pages/platform/workflow-editor/components/WorkflowEditor.tsx
@@ -1,6 +1,6 @@
 import '@xyflow/react/dist/base.css';
 import useWorkflowDataStore from '@/pages/platform/workflow-editor/stores/useWorkflowDataStore';
-import {CANVAS_BACKGROUND_COLOR} from '@/shared/constants';
+import {CANVAS_BACKGROUND_COLOR, CANVAS_TOP_OFFSET} from '@/shared/constants';
 import {
     ComponentDefinitionBasic,
     TaskDispatcherDefinitionBasic,
@@ -10,8 +10,10 @@ import {Background, BackgroundVariant, ReactFlow} from '@xyflow/react';
 import {useShallow} from 'zustand/react/shallow';
 
 import useWorkflowEditorCanvas from '../hooks/useWorkflowEditorCanvas';
+import {WorkflowEditorReadOnlyContext} from '../providers/workflowEditorReadOnlyContext';
 import NodeActionsHint from './NodeActionsHint';
 import WorkflowEditorToolbar from './WorkflowEditorToolbar';
+import WorkflowIssuesNote from './WorkflowIssuesNote';
 
 type ConditionalWorkflowEditorPropsType =
     | {
@@ -30,6 +32,8 @@ type WorkflowEditorPropsType = {
     leftSidebarOpen?: boolean;
     taskDispatcherDefinitions: TaskDispatcherDefinitionBasic[];
 };
+
+const CANVAS_DEFAULT_VIEWPORT = {x: 0, y: CANVAS_TOP_OFFSET, zoom: 1};
 
 const WorkflowEditor = ({
     componentDefinitions,
@@ -57,37 +61,40 @@ const WorkflowEditor = ({
         });
 
     return (
-        <div className="flex h-full flex-1 flex-col rounded-lg bg-background">
-            <ReactFlow
-                deleteKeyCode={null}
-                edgeTypes={edgeTypes}
-                edges={edges}
-                maxZoom={1.5}
-                minZoom={0.001}
-                nodeTypes={nodeTypes}
-                nodes={nodes}
-                nodesConnectable={false}
-                nodesDraggable={!readOnlyWorkflow}
-                onDragOver={onDragOver}
-                onDrop={onDrop}
-                onEdgesChange={onEdgesChange}
-                onNodeDragStart={handleNodeDragStart}
-                onNodeDragStop={handleNodeDragStop}
-                onNodesChange={handleNodesChange}
-                panActivationKeyCode={null}
-                panOnDrag
-                panOnScroll
-                proOptions={{hideAttribution: true}}
-                zoomOnDoubleClick={false}
-                zoomOnScroll={false}
-            >
-                <Background color={CANVAS_BACKGROUND_COLOR} size={2} variant={BackgroundVariant.Dots} />
+        <WorkflowEditorReadOnlyContext.Provider value={!!readOnlyWorkflow}>
+            <div className="flex h-full flex-1 flex-col rounded-lg bg-background">
+                <ReactFlow
+                    defaultViewport={CANVAS_DEFAULT_VIEWPORT}
+                    deleteKeyCode={null}
+                    edgeTypes={edgeTypes}
+                    edges={edges}
+                    maxZoom={1.5}
+                    minZoom={0.001}
+                    nodeTypes={nodeTypes}
+                    nodes={nodes}
+                    nodesConnectable={false}
+                    nodesDraggable={!readOnlyWorkflow}
+                    onDragOver={onDragOver}
+                    onDrop={onDrop}
+                    onEdgesChange={onEdgesChange}
+                    onNodeDragStart={handleNodeDragStart}
+                    onNodeDragStop={handleNodeDragStop}
+                    onNodesChange={handleNodesChange}
+                    panActivationKeyCode={null}
+                    panOnDrag
+                    panOnScroll
+                    proOptions={{hideAttribution: true}}
+                    zoomOnDoubleClick={false}
+                    zoomOnScroll={false}
+                >
+                    <Background color={CANVAS_BACKGROUND_COLOR} size={2} variant={BackgroundVariant.Dots} />
 
-                {!readOnlyWorkflow && nodes.length > 0 && <NodeActionsHint />}
+                    {!readOnlyWorkflow && nodes.length > 0 && <WorkflowIssuesNote fallback={<NodeActionsHint />} />}
 
-                <WorkflowEditorToolbar enableUndoRedo={enableUndoRedo} readOnly={!!readOnlyWorkflow} />
-            </ReactFlow>
-        </div>
+                    <WorkflowEditorToolbar enableUndoRedo={enableUndoRedo} readOnly={!!readOnlyWorkflow} />
+                </ReactFlow>
+            </div>
+        </WorkflowEditorReadOnlyContext.Provider>
     );
 };
 
